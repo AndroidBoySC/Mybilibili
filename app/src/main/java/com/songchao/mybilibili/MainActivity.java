971958acfe3412df714301aec9bb67edf3fd5f8d
songchao <1620674599@qq.com>
2017-07-25 10:29:34 +0800

    加入启动页与再按一次返回键退出功能

--- app/src/main/java/com/songchao/mybilibili/MainActivity.java
+++ app/src/main/java/com/songchao/mybilibili/MainActivity.java
@@ -1,13 +1,32 @@
 package com.songchao.mybilibili;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    private long lastPressTime = 0;
+    //ActionBar mActionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        //隐藏Actionbar
+        //mActionBar = getSupportActionBar();
+        //mActionBar.hide();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(new Date().getTime() - lastPressTime <2000){
+            finish();
+        }else {
+            lastPressTime = new Date().getTime();
+            Toast.makeText(this,"再按一次返回键退出",Toast.LENGTH_SHORT).show();
+        }
     }
 }
---
+++ app/src/main/java/com/songchao/mybilibili/activity/LoadActivity.java
@@ -0,0 +1,41 @@
+package com.songchao.mybilibili.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.songchao.mybilibili.MainActivity;
+import com.songchao.mybilibili.R;
+
+/**
+ * Author: SongCHao
+ * Date: 2017/7/24/16:54
+ * Email: 15704762346@163.com
+ */
+
+public class LoadActivity extends AppCompatActivity{
+    private static final int LOAD_DISPLAY_TIME = 2500;
+    //ActionBar mActionBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //getWindow().setFormat(PixelFormat.RGBA_8888);
+        //getWindow().addFlags(WindowManager.LayoutParams.FLAG_DITHER);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.load);
+        //隐藏Actionbar
+        //mActionBar = getSupportActionBar();
+        //mActionBar.hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(LoadActivity.this, MainActivity.class);
+                LoadActivity.this.startActivity(mainIntent);
+                LoadActivity.this.finish();
+            }
+        },LOAD_DISPLAY_TIME);
+    }
+}
