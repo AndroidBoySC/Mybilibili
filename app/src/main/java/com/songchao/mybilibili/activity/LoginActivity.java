9af23228cea09966b8e4e4a5f08b864234b0ebf0
songchao <1620674599@qq.com>
2017-08-08 15:00:29 +0800

    完成登录与注册界面的逻辑处理

--- app/src/main/java/com/songchao/mybilibili/activity/LoginActivity.java
+++ app/src/main/java/com/songchao/mybilibili/activity/LoginActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.songchao.mybilibili.R;
 
@@ -15,6 +17,8 @@ public class LoginActivity extends AppCompatActivity {
     private TextInputLayout userNameWraper;
     private TextInputLayout passwordWraper;
     private Button loginButton,registerButton;
+//    private String loginUserName;
+//    private String loginPassWord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +35,8 @@ public class LoginActivity extends AppCompatActivity {
         passwordWraper = (TextInputLayout) findViewById(R.id.passwordwraper);
         userNameWraper.setHint("用户名");
         passwordWraper.setHint("密码");
+//        loginUserName = userNameWraper.getEditText().getText().toString().trim();
+//        loginPassWord = passwordWraper.getEditText().getText().toString().trim();
 
         loginButton = (Button) findViewById(R.id.login);
 
@@ -39,8 +45,51 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent,1);
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        switch (resultCode){
+//            case 705128:
+//                Bundle bundle = data.getExtras();
+//                String username = bundle.getString("username");
+//                String password = bundle.getString("password");
+//                if(username.equals(loginUserName)&&password.equals(loginPassWord)){
+//                    finish();
+//                    //后续可以利用广播更改主界面文字为设置头像提醒
+//                }else {
+//                    Toast.makeText(LoginActivity.this,"用户名或密码不正确",Toast.LENGTH_SHORT).show();
+//                }
+//                break;
+//            default:
+//                break;
+        if(requestCode == 1&&resultCode == 705128){
+            Bundle bundle = data.getExtras();
+            final String username = bundle.getString("username");
+            final String password = bundle.getString("password");
+
+                loginButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if(userNameWraper.getEditText().getText().toString().trim().equals(username)&&
+                                passwordWraper.getEditText().getText().toString().trim().equals(password)){
+                            Log.d("登录界面输入用户名", userNameWraper.getEditText().getText().toString().trim());
+                            Log.d("登录界面输入密码", passwordWraper.getEditText().getText().toString().trim());
+                            finish();
+                            // TODO: 2017/8/8 后期可以添加广播更新主界面为已登录并提示用户可设置头像
+                        }else {
+                            Log.d("登录界面输入用户名", userNameWraper.getEditText().getText().toString().trim());
+                            Log.d("登录界面输入密码", passwordWraper.getEditText().getText().toString().trim());
+                            Toast.makeText(LoginActivity.this,"用户名或密码错误",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+        }
+    }
+
 }
+
--- app/src/main/java/com/songchao/mybilibili/activity/RegisterActivity.java
+++ app/src/main/java/com/songchao/mybilibili/activity/RegisterActivity.java
@@ -1,8 +1,10 @@
 package com.songchao.mybilibili.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -13,6 +15,8 @@ public class RegisterActivity extends AppCompatActivity {
     private TextInputLayout userName,passWord;
     private Button registerButton;
     private ImageView backImageView;
+//    private SharedPreferences mPreferences;
+//    private SharedPreferences.Editor mEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +31,29 @@ public class RegisterActivity extends AppCompatActivity {
         });
         userName = (TextInputLayout) findViewById(R.id.register_user);
         passWord = (TextInputLayout) findViewById(R.id.register_password);
-        String textUserName = userName.getEditText().getText().toString();
-        String textPassWord = passWord.getEditText().getText().toString();
+//        final String textUserName = userName.getEditText().getText().toString().trim();
+//        final String textPassWord = passWord.getEditText().getText().toString().trim();
         registerButton = (Button) findViewById(R.id.register_success);
+//        mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+//        String name = mPreferences.getString("userName", textUserName);
+//        String pass = mPreferences.getString("passWord",textPassWord);
+//        mEditor = mPreferences.edit();
+//        mEditor.putString("username",name);
+//        mEditor.putString("password",pass);
+//        mEditor.apply();
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                Bundle bundle = new Bundle();
+                bundle.putString("username",userName.getEditText().getText().toString().trim());
+                bundle.putString("password",passWord.getEditText().getText().toString().trim());
+                Log.d("注册界面输入用户名", userName.getEditText().getText().toString().trim());
+                Log.d("注册界面输入密码", passWord.getEditText().getText().toString().trim());
+                intent.putExtras(bundle);
+                setResult(705128,intent);
+                finish();
+            }
+        });
     }
 }
