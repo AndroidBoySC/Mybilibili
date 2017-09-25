524cf6413947a11d7fcea4dceab08449502b15e9
songchao <1620674599@qq.com>
2017-09-25 16:30:27 +0800

    启动页替换为视频

--- app/src/main/java/com/songchao/mybilibili/activity/LoadActivity.java
+++ app/src/main/java/com/songchao/mybilibili/activity/LoadActivity.java
@@ -1,12 +1,14 @@
 package com.songchao.mybilibili.activity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
 
 import com.songchao.mybilibili.R;
+import com.songchao.mybilibili.view.CustomVideoView;
 
 /**
  * Author: SongCHao
@@ -15,8 +17,9 @@ import com.songchao.mybilibili.R;
  */
 
 public class LoadActivity extends AppCompatActivity{
-    private static final int LOAD_DISPLAY_TIME = 2500;
+    private static final int LOAD_DISPLAY_TIME = 10000;
     //ActionBar mActionBar;
+    private CustomVideoView mCustomVideoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,11 @@ public class LoadActivity extends AppCompatActivity{
         //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         //requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.load);
+        mCustomVideoView = (CustomVideoView) findViewById(R.id.my_video_view);
+        //设置播放加载路径
+        mCustomVideoView.setVideoURI(Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.baiwei));
+        //播放
+        mCustomVideoView.start();
         //隐藏Actionbar
         //mActionBar = getSupportActionBar();
         //mActionBar.hide();
