f576e3a32593939cd75cb8af8b256807a02450b3
songchao <1620674599@qq.com>
2017-09-26 16:20:46 +0800

    初步集成高德地图

--- app/src/main/java/com/songchao/mybilibili/activity/ThemeActivity.java
+++ app/src/main/java/com/songchao/mybilibili/activity/ThemeActivity.java
@@ -3,13 +3,48 @@ package com.songchao.mybilibili.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
 import com.songchao.mybilibili.R;
 
 public class ThemeActivity extends AppCompatActivity {
+    MapView mMapView = null;
+    AMap mAMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_theme);
+        mMapView = (MapView) findViewById(R.id.mapview);
+        mMapView.onCreate(savedInstanceState);
+        if (mAMap == null){
+            mAMap = mMapView.getMap();
+        }
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+        mMapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+        mMapView.onPause();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+        mMapView.onSaveInstanceState(outState);
+    }
+
 }
