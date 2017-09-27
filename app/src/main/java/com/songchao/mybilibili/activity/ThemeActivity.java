e7e0bb6183442dfca01f039cbc403ca1175d6b02
songchao <1620674599@qq.com>
2017-09-27 16:57:19 +0800

    完成定位功能

--- app/src/main/java/com/songchao/mybilibili/activity/ThemeActivity.java
+++ app/src/main/java/com/songchao/mybilibili/activity/ThemeActivity.java
@@ -5,11 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.MyLocationStyle;
 import com.songchao.mybilibili.R;
 
 public class ThemeActivity extends AppCompatActivity {
     MapView mMapView = null;
     AMap mAMap;
+    MyLocationStyle mMyLocationStyle;
+    //高德地图SDK5.0之后定位不需要接入定位SDK
+//    public AMapLocationClient mAMapLocationClient;
+//    public AMapLocationClientOption mAMapLocationClientOption = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +26,57 @@ public class ThemeActivity extends AppCompatActivity {
         if (mAMap == null){
             mAMap = mMapView.getMap();
         }
+//        mAMapLocationClient = new AMapLocationClient(this);
+//        mAMapLocationClientOption = new AMapLocationClientOption();
+//        mAMapLocationClient.setLocationListener(new AMapLocationListener() {
+//            @Override
+//            public void onLocationChanged(AMapLocation aMapLocation) {
+//                if (aMapLocation != null) {
+//                    if (aMapLocation.getErrorCode() == 0) {
+//                        //定位成功回调信息，设置相关消息
+//                        aMapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见定位类型表
+//                        aMapLocation.getLatitude();//获取纬度
+//                        aMapLocation.getLongitude();//获取经度
+//                        aMapLocation.getAccuracy();//获取精度信息
+//                        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//                        Date date = new Date(aMapLocation.getTime());
+//                        df.format(date);//定位时间
+//                    } else {
+//                        //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
+//                        Log.e("AmapError","location Error, ErrCode:"
+//                                + aMapLocation.getErrorCode() + ", errInfo:"
+//                                + aMapLocation.getErrorInfo());
+//                    }
+//                }
+//
+//            }
+//        });
+//        mAMapLocationClientOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Battery_Saving);
+//        mAMapLocationClientOption.setInterval(2000);
+//        mAMapLocationClient.setLocationOption(mAMapLocationClientOption);
+//        mAMapLocationClient.startLocation();
+        mMyLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        mMyLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        mAMap.setMyLocationStyle(mMyLocationStyle);//设置定位蓝点的Style
+        mAMap.getUiSettings().setMyLocationButtonEnabled(true);//设置默认定位按钮是否显示，非必需设置。
+        mAMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+        mMyLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）默认执行此种模式
+        //显示蓝点
+        mMyLocationStyle.showMyLocation(true);
+        //显示室内
+        //mAMap.showIndoorMap(true);
+        //下面这样是可以通过自定义设置蓝点图标什么的
+//        mMyLocationStyle.myLocationIcon(BitmapDescriptorFactory.fromResource(R.mipmap.showlocation));
+//        mAMap.setMyLocationStyle(mMyLocationStyle);
     }
+
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
         mMapView.onDestroy();
+        //mAMapLocationClient.stopLocation();
     }
     @Override
     protected void onResume() {
