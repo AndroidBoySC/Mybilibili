b15aee2494ba58fba81eb39cbce7977ed0d4e812
songchao <1620674599@qq.com>
2017-09-27 17:35:53 +0800

    增加切换地图模式

--- app/src/main/java/com/songchao/mybilibili/activity/ThemeActivity.java
+++ app/src/main/java/com/songchao/mybilibili/activity/ThemeActivity.java
@@ -2,6 +2,8 @@ package com.songchao.mybilibili.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.MapView;
@@ -12,6 +14,7 @@ public class ThemeActivity extends AppCompatActivity {
     MapView mMapView = null;
     AMap mAMap;
     MyLocationStyle mMyLocationStyle;
+    private Button mButton,mButton1,mButton2;
     //高德地图SDK5.0之后定位不需要接入定位SDK
 //    public AMapLocationClient mAMapLocationClient;
 //    public AMapLocationClientOption mAMapLocationClientOption = null;
@@ -56,7 +59,7 @@ public class ThemeActivity extends AppCompatActivity {
 //        mAMapLocationClient.setLocationOption(mAMapLocationClientOption);
 //        mAMapLocationClient.startLocation();
         mMyLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
-        mMyLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        mMyLocationStyle.interval(50000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
         mAMap.setMyLocationStyle(mMyLocationStyle);//设置定位蓝点的Style
         mAMap.getUiSettings().setMyLocationButtonEnabled(true);//设置默认定位按钮是否显示，非必需设置。
         mAMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
@@ -69,6 +72,7 @@ public class ThemeActivity extends AppCompatActivity {
 //        mMyLocationStyle.myLocationIcon(BitmapDescriptorFactory.fromResource(R.mipmap.showlocation));
 //        mAMap.setMyLocationStyle(mMyLocationStyle);
     }
+
 
 
     @Override
@@ -97,4 +101,15 @@ public class ThemeActivity extends AppCompatActivity {
         mMapView.onSaveInstanceState(outState);
     }
 
+    public void Dream(View view) {
+        mAMap.setMapType(AMap.MAP_TYPE_SATELLITE);
+    }
+
+    public void Qing(View view) {
+        mAMap.setMapType(AMap.MAP_TYPE_NIGHT);
+    }
+
+    public void Sheng(View view) {
+        mAMap.setMapType(AMap.MAP_TYPE_NORMAL);
+    }
 }
