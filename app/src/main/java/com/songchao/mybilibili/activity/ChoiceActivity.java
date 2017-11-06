d1c6fdff8740862910be8e464557667f7a2f444d
songchao <1620674599@qq.com>
2017-11-06 09:59:13 +0800

    完善选择城市地区

--- app/src/main/java/com/songchao/mybilibili/activity/ChoiceActivity.java
+++ app/src/main/java/com/songchao/mybilibili/activity/ChoiceActivity.java
@@ -3,7 +3,6 @@ package com.songchao.mybilibili.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
@@ -11,9 +10,6 @@ import com.songchao.mybilibili.R;
 
 public class ChoiceActivity extends AppCompatActivity {
     private RadioGroup mRadioGroup;
-    private RadioButton mRadioButtonChina,mRadioButtonHongKong,mRadioButtonMacau,mRadioButtonTaiwan,
-    mRadioButtonAmerica,mRadioButtonBelgium,mRadioButtonAustrilia,mRadioButtonFrance,mRadioButtonJapan,
-    mRadioButtonSingapore,mRadioButtonRngland;
     private TextView mTextViewCancel;
 
 
@@ -38,43 +34,53 @@ public class ChoiceActivity extends AppCompatActivity {
                         finish();
                         break;
                     case R.id.rb_choice_macau:
-
+                        Intent intent2 = new Intent("com.songchao.mybilibili.choicethree");
+                        sendBroadcast(intent2);
                         finish();
                         break;
                     case R.id.rb_choice_taiwan:
-
+                        Intent intent3 = new Intent("com.songchao.mybilibili.choicefour");
+                        sendBroadcast(intent3);
                         finish();
                         break;
                     case R.id.rb_choice_America:
-
+                        Intent intent4 = new Intent("com.songchao.mybilibili.choicefive");
+                        sendBroadcast(intent4);
                         finish();
                         break;
                     case R.id.rb_choice_Belgium:
-
+                        Intent intent5 = new Intent("com.songchao.mybilibili.choicesix");
+                        sendBroadcast(intent5);
                         finish();
                         break;
                     case R.id.rb_choice_Australia:
-
+                        Intent intent6 = new Intent("com.songchao.mybilibili.choiceseven");
+                        sendBroadcast(intent6);
                         finish();
                         break;
                     case R.id.rb_choice_France:
-
+                        Intent intent7 = new Intent("com.songchao.mybilibili.choiceeight");
+                        sendBroadcast(intent7);
                         finish();
                         break;
                     case R.id.rb_choice_Canada:
-
+                        Intent intent8 = new Intent("com.songchao.mybilibili.choicenine");
+                        sendBroadcast(intent8);
                         finish();
                         break;
                     case R.id.rb_choice_Japan:
-
+                        Intent intent9 = new Intent("com.songchao.mybilibili.choiceten");
+                        sendBroadcast(intent9);
                         finish();
                         break;
                     case R.id.rb_choice_Singapore:
-
+                        Intent intent10 = new Intent("com.songchao.mybilibili.choiceeleven");
+                        sendBroadcast(intent10);
                         finish();
                         break;
                     case R.id.rb_choice_England:
-
+                        Intent intent11 = new Intent("com.songchao.mybilibili.choicetwelve");
+                        sendBroadcast(intent11);
                         finish();
                         break;
                         default:
@@ -89,17 +95,6 @@ public class ChoiceActivity extends AppCompatActivity {
 
     private void initView() {
         mRadioGroup = findViewById(R.id.rg_choice);
-        mRadioButtonChina = findViewById(R.id.rb_choice_china);
-        mRadioButtonHongKong = findViewById(R.id.rb_choice_HongKong);
-        mRadioButtonMacau = findViewById(R.id.rb_choice_macau);
-        mRadioButtonTaiwan = findViewById(R.id.rb_choice_taiwan);
-        mRadioButtonAmerica = findViewById(R.id.rb_choice_America);
-        mRadioButtonBelgium = findViewById(R.id.rb_choice_Belgium);
-        mRadioButtonAustrilia = findViewById(R.id.rb_choice_Australia);
-        mRadioButtonFrance = findViewById(R.id.rb_choice_France);
-        mRadioButtonJapan = findViewById(R.id.rb_choice_Japan);
-        mRadioButtonSingapore = findViewById(R.id.rb_choice_Singapore);
-        mRadioButtonRngland = findViewById(R.id.rb_choice_England);
         mTextViewCancel = findViewById(R.id.tv_choice_cancel);
     }
 
--- app/src/main/java/com/songchao/mybilibili/activity/RegisterActivity.java
+++ app/src/main/java/com/songchao/mybilibili/activity/RegisterActivity.java
@@ -27,6 +27,16 @@ public class RegisterActivity extends AppCompatActivity {
         mIntentFilter = new IntentFilter();
         mIntentFilter.addAction("com.songchao.mybilibili.choiceone");
         mIntentFilter.addAction("com.songchao.mybilibili.choicetwo");
+        mIntentFilter.addAction("com.songchao.mybilibili.choicethree");
+        mIntentFilter.addAction("com.songchao.mybilibili.choicefour");
+        mIntentFilter.addAction("com.songchao.mybilibili.choicefive");
+        mIntentFilter.addAction("com.songchao.mybilibili.choicesix");
+        mIntentFilter.addAction("com.songchao.mybilibili.choiceseven");
+        mIntentFilter.addAction("com.songchao.mybilibili.choiceeight");
+        mIntentFilter.addAction("com.songchao.mybilibili.choicenine");
+        mIntentFilter.addAction("com.songchao.mybilibili.choiceten");
+        mIntentFilter.addAction("com.songchao.mybilibili.choiceeleven");
+        mIntentFilter.addAction("com.songchao.mybilibili.choicetwelve");
         mChoiceReceiver = new ChoiceReceiver();
         registerReceiver(mChoiceReceiver,mIntentFilter);
         mTextViewTitle.setText("注册帐号");
@@ -52,9 +62,29 @@ public class RegisterActivity extends AppCompatActivity {
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
             if (action.equals("com.songchao.mybilibili.choiceone")){
-                return;
+                mTextViewChoic.setText("中国大陆");
             }else if (action.equals("com.songchao.mybilibili.choicetwo")){
                 mTextViewChoic.setText("香港特别行政区");
+            }else if (action.equals("com.songchao.mybilibili.choicethree")){
+                mTextViewChoic.setText("澳门特别行政区");
+            }else if (action.equals("com.songchao.mybilibili.choicefour")){
+                mTextViewChoic.setText("台湾地区");
+            }else if (action.equals("com.songchao.mybilibili.choicefive")){
+                mTextViewChoic.setText("美国");
+            }else if (action.equals("com.songchao.mybilibili.choicesix")){
+                mTextViewChoic.setText("比利时");
+            }else if (action.equals("com.songchao.mybilibili.choiceseven")){
+                mTextViewChoic.setText("澳大利亚");
+            }else if (action.equals("com.songchao.mybilibili.choiceeight")){
+                mTextViewChoic.setText("法国");
+            }else if (action.equals("com.songchao.mybilibili.choicenine")){
+                mTextViewChoic.setText("加拿大");
+            }else if (action.equals("com.songchao.mybilibili.choiceten")){
+                mTextViewChoic.setText("日本");
+            }else if (action.equals("com.songchao.mybilibili.choiceeleven")){
+                mTextViewChoic.setText("新加坡");
+            }else if(action.equals("com.songchao.mybilibili.choicetwelve")){
+                mTextViewChoic.setText("英国");
             }
         }
     }
